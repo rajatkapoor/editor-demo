140f3622a6294f6722c907af9f6646b7fdbbc8df
rajatkapoor <rajat@hashnode.com>
2023-10-03 14:48:54 +0530

    Saving content in indexDB

--- src/components/Editor/Tiptap.tsx
+++ src/components/Editor/Tiptap.tsx
@@ -1,20 +1,31 @@
 import TweetNode from "@/lib/Editor/extensions/tweet";
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import { set, get } from "idb-keyval";
+import { useEffect } from "react";
+
+const contentKey = `EditorContent`;
 
 const Editor = ({ onUpdate }: { onUpdate: (htmlContent: string) => void }) => {
   const editor = useEditor({
     extensions: [StarterKit, TweetNode],
-    content: `<p>Hello React India 👋</p><tweet tweetId="1708932196285784499"></tweet><p></p>`,
     onUpdate: ({ editor }) => {
-      onUpdate(editor.getHTML());
+      const htmlContent = editor.getHTML();
+      set(contentKey, htmlContent);
+      onUpdate(htmlContent);
     },
   });
+
+  useEffect(() => {
+    get(contentKey).then((content) => {
+      editor?.commands.setContent(content);
+    });
+  }, [editor]);
 
   return (
     <EditorContent
       editor={editor}
-      className="h-full border border-slate-600 rounded-md p-4 prose m-2 focus-visible:outline-none "
+      className="h-full border border-slate-600 rounded-md p-4 prose focus-visible:outline-none"
     />
   );
 };
