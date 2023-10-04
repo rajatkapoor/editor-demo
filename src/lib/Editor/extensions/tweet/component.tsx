0b99c467f8b2fd1f9192de0f36f266f54735d396
rajatkapoor <rajat@hashnode.com>
2023-10-04 20:45:10 +0530

    Added option to edit tweet

--- src/lib/Editor/extensions/tweet/component.tsx
+++ src/lib/Editor/extensions/tweet/component.tsx
@@ -4,7 +4,7 @@ import {
   NodeViewRendererProps,
   NodeViewWrapper,
 } from "@tiptap/react";
-import React from "react";
+import React, { useState } from "react";
 import { Tweet } from "react-tweet";
 import { useForm, SubmitHandler } from "react-hook-form";
 
@@ -13,20 +13,21 @@ type Inputs = {
   exampleRequired: string;
 };
 
-const Input: React.FC<{ onUpdate: (tweetId: string) => void }> = ({
-  onUpdate,
+const Input: React.FC<{ onSubmit: (tweetId: string) => void }> = ({
+  onSubmit,
 }) => {
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = (data) => onUpdate(data.tweetId);
+  const onSubmitHandler: SubmitHandler<Inputs> = (data) =>
+    onSubmit(data.tweetId);
 
   return (
     /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
     <form
-      onSubmit={handleSubmit(onSubmit)}
+      onSubmit={handleSubmit(onSubmitHandler)}
       className="flex flex-row gap-2 w-full  justify-between border border-slate-400 rounded-md p-1"
     >
       {/* register your input into the hook by invoking the "register" function */}
@@ -44,21 +45,36 @@ const Input: React.FC<{ onUpdate: (tweetId: string) => void }> = ({
   );
 };
 
-const TweetComponent = ({ node, updateAttributes }: NodeViewProps) => {
+const TweetComponent = ({
+  node,
+  updateAttributes,
+  selected,
+}: NodeViewProps) => {
   const { tweetId } = node.attrs;
+  const [mode, setMode] = useState<"view" | "edit">(tweetId ? "view" : "edit");
 
   const tweetIdString = tweetId;
+  const onSubmit = (tweetId: string) => {
+    updateAttributes({ tweetId });
+    setMode("view");
+  };
 
   return (
-    <NodeViewWrapper className="react-component">
-      {tweetId && (
+    <NodeViewWrapper className="react-component relative">
+      {tweetId && mode === "view" && (
         <div data-theme="light">
           <Tweet id={tweetIdString} />
         </div>
       )}
-      {!tweetId && (
-        <Input onUpdate={(tweetId) => updateAttributes({ tweetId })} />
+      {selected && mode === "view" && (
+        <button
+          className="border bg-white border-blue-400 rounded-md px-2 top-2 right-2  absolute"
+          onClick={() => setMode("edit")}
+        >
+          Edit
+        </button>
       )}
+      {mode === "edit" && <Input onSubmit={onSubmit} />}
     </NodeViewWrapper>
   );
 };
